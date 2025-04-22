27f56faab2c1fb87d0c272fbf4659f16093b80c2
AlwaysMori <116044642+AlwaysMori@users.noreply.github.com>
2025-04-22 21:56:17 +0700

    feat: Update page title to 'My Nimu' for consistency across the application

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header class="bg-gray-800 text-white border-b-2 border-blue-700 shadow-md" style="font-family: 'Poppins', sans-serif;">
     <div class="container mx-auto flex justify-between items-center p-4">
-        <h1 class="text-xl font-bold">{{ $title ?? 'Default Title' }}</h1>
+        <h1 class="text-xl  font-light">My Nimu</h1>
         <button id="menu-toggle" class="block md:hidden text-white focus:outline-none">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
